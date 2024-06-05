fcd9df83354fa5535ce302f9d989d4917d2f4a11
giovannijunseokim <kingjunseo@gmail.com>
2024-06-05 16:03:42 +0900

    init project

---
+++ src/main/java/com/gojap/moviechu/MovieChuApplication.java
@@ -0,0 +1,13 @@
+package com.gojap.moviechu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieChuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieChuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gojap/moviechu/MovieChuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gojap.moviechu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieChuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
